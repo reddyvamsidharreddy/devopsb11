46e20ee8389d3f21c039728563e23f1fa80e8f36
vamsidevops <vamsidhary.yeddyla@gmail.com>
2020-02-20 22:36:42 +0530

    master

--- even.sh
+++ even.sh
@@ -1,13 +1,7 @@
-#!/bin/bash
+#!bin/bash
 #This will give all name from 1 to 100 this is linux user addwd by windows
  firstgit
 #this is from git 
-$a = 5
-$b = 6
-$c = $a + $b
-echo "$c"
-
- master
 for i in {1..100}
 do
 if [ $(expr $i % 2) -eq 0 ]
@@ -22,8 +16,4 @@ done
 func(){
     echo "This is a test function"
 }
- firstgit
-#This is from firstgit master
-
-#This is from "MASTER"
  master
