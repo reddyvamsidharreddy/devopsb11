1fb86d344f1b25be95631981197c4ddbe0fd04dd
vamsidevops <vamsidhary.yeddyla@gmail.com>
2020-02-20 21:44:13 +0530

    modified

--- even.sh
+++ even.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #This will give all name from 1 to 100 this is linux user addwd by windows
-#this is from git 
 for i in {1..100}
 do
 if [ $(expr $i % 2) -eq 0 ]
@@ -15,4 +14,4 @@ done
 func(){
     echo "This is a test function"
 }
-#This is from master
+#This is from "MASTER"
